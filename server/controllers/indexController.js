d591f6f812dee4193c590b5c923a6227fc7d8aca
ronewa <ronewamavhungu@gmail.com>
2022-05-22 17:56:43 +0200

    validation

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,3 +1,6 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+
 const login_get = async (req,res)=>{
     console.log(req.session);
     const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
@@ -5,7 +8,25 @@ const login_get = async (req,res)=>{
 };
 const login_post = async (req, res)=>{
     const {email, password} = req.body;
-    
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      req.session.error = "Invalid Credentials";
+      return res.json({"error":"Invalid Credentials"});
+    }
+  
+    const isMatch = await bcrypt.compare(password, user.password);
+  
+    if (!isMatch) {
+      req.session.error = "Invalid Credentials";
+      return res.json({"error":"Invalid Credentials"});
+    }
+  
+    req.session.isAuth = true;
+    req.session.username = user.username;
+    res.json({"message":"/dashboard"});
+
 }
 const register_get = (req, res)=>{
     const error = req.session.error;
@@ -14,15 +35,24 @@ const register_get = (req, res)=>{
 };
 const register_post = async (req, res)=>{
 
-    const { email, password } = req.body;
-    //Check if user already exists
+    const { username, email, password } = req.body;
 
-    //const user = await User.find({email})
-    if(user){
-        req.session.error("User already exist");
-        return res.json();
+    let user = await User.findOne({ email });
+  
+    if (user) {
+      req.session.error = "User already exists";
+      return res.json();
     }
-    return res.json();
+  
+    const hasdPsw = await bcrypt.hash(password, 12);
+  
+    user = new User({
+      username,
+      email,
+      password: hasdPsw,
+    });
+  
+    await user.save();
 };
 
 module.exports = {login_get, login_post, register_post, register_get}
