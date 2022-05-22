49eab8cc3b0388f05bbccaa6696e81d1f6f29f79
ronewa <ronewamavhungu@gmail.com>
2022-05-22 16:59:30 +0200

    adding sessions

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
 const logger = require('morgan');
 
 const notFound = require('./middleware/not-found');
@@ -12,14 +18,35 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+const  store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: 'mySessions'
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
+app.use(cors({
+  origin : ['http://localhost:3000'],
+  credentials: true
+}));
+app.use(session({
+  secret: 'keyboard_ronewa',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    maxAge: 1000 * 60 * 60 * 24 * 7, 
+    //secure: true 
+  },
+  store: store,
+}))
+app.use(mongoSanitize())
+app.use(xss());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/', indexRouter);
--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,4 +1,5 @@
 const login = async (req,res)=>{
+    console.log(req.session);
     const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     res.render('index', { title: 'Express', ipaddress });
 }
