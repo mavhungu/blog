674756c58797efcd79a41ce4bb8e23fb04aada60
ronewa <ronewamavhungu@gmail.com>
2022-05-22 17:36:22 +0200

    linking routers

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,7 +1,28 @@
-const login = async (req,res)=>{
+const login_get = async (req,res)=>{
     console.log(req.session);
     const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     res.render('index', { title: 'Express', ipaddress });
+};
+const login_post = async (req, res)=>{
+    const {email, password} = req.body;
+    
 }
+const register_get = (req, res)=>{
+    const error = req.session.error;
+    delete req.session.error;
+    res.json();
+};
+const register_post = async (req, res)=>{
 
-module.exports = {login}
+    const { email, password } = req.body;
+    //Check if user already exists
+
+    //const user = await User.find({email})
+    if(user){
+        req.session.error("User already exist");
+        return res.json();
+    }
+    return res.json();
+};
+
+module.exports = {login_get, login_post, register_post, register_get}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {login} = require('../controllers/indexController');
+const {login_get, login_post, register_post, register_get} = require('../controllers/indexController');
 
 /* GET home page. */
-router.route('/').get(login)
+router.route('/').get(login_get).post(login_post)
 
-router.route('/singout').get()
+router.route('/register').get(register_get).post(register_post)
 
 module.exports = router;
