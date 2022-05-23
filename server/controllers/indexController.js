8dcb99d8c02668093ae09c7905edf64559f451b6
ronewa <ronewamavhungu@gmail.com>
2022-05-23 13:24:15 +0200

    ...

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,58 +1,56 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
-const login_get = async (req,res)=>{
-    console.log(req.session);
-    const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    res.render('index', { title: 'Express', ipaddress });
+const login_get = async (req, res) => {
+  console.log(req.session);
+  const ipaddress = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  res.render("index", { title: "Express", ipaddress });
 };
-const login_post = async (req, res)=>{
-    const {email, password} = req.body;
+const login_post = async (req, res) => {
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (!user) {
-      req.session.error = "Invalid Credentials";
-      return res.json({"error":"Invalid Credentials"});
-    }
-  
-    const isMatch = await bcrypt.compare(password, user.password);
-  
-    if (!isMatch) {
-      req.session.error = "Invalid Credentials";
-      return res.json({"error":"Invalid Credentials"});
-    }
-  
-    req.session.isAuth = true;
-    req.session.username = user.username;
-    res.json({"message":"/dashboard"});
+  if (!user) {
+    req.session.error = "Invalid Credentials";
+    return res.json({ error: "Invalid Credentials" });
+  }
 
-}
-const register_get = (req, res)=>{
-    const error = req.session.error;
-    delete req.session.error;
-    res.json();
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    req.session.error = "Invalid Credentials";
+    return res.json({ error: "Invalid Credentials" });
+  }
+
+  req.session.isAuth = true;
+  req.session.username = user.username;
+  res.json({ message: "/dashboard" });
 };
-const register_post = async (req, res)=>{
+const register_get = (req, res) => {
+  const error = req.session.error;
+  delete req.session.error;
+  res.json();
+};
+const register_post = async (req, res) => {
+  const { username, email, password } = req.body;
 
-    const { username, email, password } = req.body;
+  let user = await User.findOne({ email });
 
-    let user = await User.findOne({ email });
-  
-    if (user) {
-      req.session.error = "User already exists";
-      return res.json();
-    }
-  
-    const hasdPsw = await bcrypt.hash(password, 12);
-  
-    user = new User({
-      username,
-      email,
-      password: hasdPsw,
-    });
-  
-    await user.save();
+  if (user) {
+    req.session.error = "User already exists";
+    return res.json();
+  }
+
+  const hasdPsw = await bcrypt.hash(password, 12);
+
+  user = new User({
+    username,
+    email,
+    password: hasdPsw,
+  });
+
+  await user.save();
 };
 
-module.exports = {login_get, login_post, register_post, register_get}
+module.exports = { login_get, login_post, register_post, register_get };
